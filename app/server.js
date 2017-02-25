793f9ca3e2cc56d75bf8008fd3d4a75a6afa4852
german gomez <germangb42@gmail.com>
2017-02-25 01:27:34 +0100

    initial commit

---
+++ app/server.js
@@ -0,0 +1,21 @@
+express = require('express')
+session = require('express-session')
+redis_store = require('connect-redis')(session)
+
+app = express()
+
+redis_opt = {
+    host: 'redis',
+    port: 6379
+}
+
+app.use(session({
+    secret: 'foo',
+    store: new redis_store(redis_opt)
+}))
+
+app.get('/', function(req, res) {
+    res.send('Hello node!!!!! :D')
+})
+
+app.listen(8888)
